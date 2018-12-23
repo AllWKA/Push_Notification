57a8b098459a1690fa7105bc11f23d02fe20eb67
AllWKa <bryanjaramillobaldeon@gmail.com>
2018-12-23 17:08:17 +0000

    merry chrismas

--- api_notifications_push/News_With_Push_Notifications/src/libs/boot.js
+++
@@ -1,6 +0,0 @@
-module.exports = app => {
-
-    app.listen(app.get('port'), () => {
-        
-    });
-};
--- api_notifications_push/News_With_Push_Notifications/src/libs/config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    database: 'db_notifications_push',
-    username: 'root',
-    password: 'Admin123',
-    params: {
-        dialect: 'mysql'
-    }
-};
